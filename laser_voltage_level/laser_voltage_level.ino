473f7a96844e1d43f7e381d248ec666657f13183
Will Ballard <wballard@mailframe.net>
2017-02-04 11:55:33 -0600

    voltage scale

--- laser_voltage_level/laser_voltage_level.ino
+++ laser_voltage_level/laser_voltage_level.ino
@@ -12,13 +12,13 @@ void setup() {
 }
 
 int level(int val) {
-  return max(min(val / 4, 255) - 1, 0);
+  return val / 4;
 }
 
 void loop() {
   //read up to 1024, though this will max at 676 so scale it up
-  int val_0 = analogRead(0) * 5 / 3.3;
-  int val_1 = analogRead(1) * 5 / 3.3;
+  int val_0 = analogRead(0) * 1.6;
+  int val_1 = analogRead(1) * 1.6;
   analogWrite(9, level(val_0));
   analogWrite(10, level(val_1));
   digitalWrite(LED_BUILTIN, level(val_0));
